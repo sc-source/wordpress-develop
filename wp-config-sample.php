bca766d65162bf9b0d2c91b013fcd94f2eb7f778
sc-source <57882855+sc-source@users.noreply.github.com>
2025-06-17 21:37:33 +0800

    Update wp-config-sample.php

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -71,7 +71,7 @@ define( 'NONCE_SALT',       'put your unique phrase here' );
  *
  * @link https://developer.wordpress.org/advanced-administration/wordpress/wp-config/#table-prefix
  */
-$table_prefix = 'wp_';
+$table_prefix = 'wp_e2e_';
 
 /**
  * For developers: WordPress debugging mode.
